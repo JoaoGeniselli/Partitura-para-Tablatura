a1963f2026c71bab3290a4707753f13c43070381
JoaoGeniselli <joao.g.geniselli@gmail.com>
2020-08-06 21:45:39 -0300

    Note initial model structure.

--- app/src/main/java/com/dosei/chordsdictionary/GuitarNotes.kt
+++ app/src/main/java/com/dosei/chordsdictionary/GuitarNotes.kt
@@ -1,73 +1,151 @@
 package com.dosei.chordsdictionary
 
-enum class GuitarNotes {
-    C2,
-    C2_SHARP,
-    D2_FLAT,
-    D2,
-    D2_SHARP,
-    E2,
-    F2,
-    F2_SHARP,
-    G2_FLAT,
-    G2,
-    G2_SHARP,
-    A2_FLAT,
-    A2,
-    A2_SHARP,
-    B2_FLAT,
-    C3,
-    C3_SHARP,
-    D3_FLAT,
-    D3,
-    D3_SHARP,
-    E3,
-    F3,
-    F3_SHARP,
-    G3_FLAT,
-    G3,
-    G3_SHARP,
-    A3_FLAT,
-    A3,
-    A3_SHARP,
-    B3_FLAT,
-    C4,
-    C4_SHARP,
-    D4_FLAT,
-    D4,
-    D4_SHARP,
-    E4,
-    F4,
-    F4_SHARP,
-    G4_FLAT,
-    G4,
-    G4_SHARP,
-    A4_FLAT,
-    A4,
-    A4_SHARP,
-    B4_FLAT,
-    C5,
-    C5_SHARP,
-    D5_FLAT,
-    D5,
-    D5_SHARP,
-    E5,
-    F5,
-    F5_SHARP,
-    G5_FLAT,
-    G5,
-    G5_SHARP,
-    A5_FLAT,
-    A5,
-    A5_SHARP,
-    B5_FLAT,
-    C6,
-    C6_SHARP,
-    D6_FLAT,
-    D6,
-    D6_SHARP,
-    E6
-}
+data class GuitarNote(
+    val name: String,
+    val positions: GuitarPositions
+)
+
+fun initNaturalNotes(): List<GuitarNote> = listOf(
+    GuitarNote("E2", GuitarPositions(
+        chord6 = 0
+    )),
+    GuitarNote("F2", GuitarPositions(
+        chord6 = 1
+    )),
+    GuitarNote("G2", GuitarPositions(
+        chord6 = 3
+    )),
+    GuitarNote("A2", GuitarPositions(
+        chord5 = 0,
+        chord6 = 5
+    )),
+    GuitarNote("B2", GuitarPositions(
+        chord5 = 2,
+        chord6 = 7
+    )),
+    GuitarNote("C3", GuitarPositions(
+        chord5 = 3,
+        chord6 = 8
+    )),
+    GuitarNote("D3", GuitarPositions(
+        chord4 = 0,
+        chord5 = 5,
+        chord6 = 10
+    )),
+    GuitarNote("E3", GuitarPositions(
+        chord4 = 2,
+        chord5 = 7,
+        chord6 = 12
+    )),
+    GuitarNote("F3", GuitarPositions(
+        chord4 = 3,
+        chord5 = 8,
+        chord6 = 13
+    )),
+    GuitarNote("G3", GuitarPositions(
+        chord3 = 0,
+        chord4 = 5,
+        chord5 = 10,
+        chord6 = 15
+    )),
+    GuitarNote("A3", GuitarPositions(
+        chord3 = 2,
+        chord4 = 7,
+        chord5 = 12,
+        chord6 = 17
+    )),
+    GuitarNote("B3", GuitarPositions(
+        chord2 = 0,
+        chord3 = 4,
+        chord4 = 9,
+        chord5 = 14,
+        chord6 = 19
+    )),
+    GuitarNote("C4", GuitarPositions(
+        chord2 = 1,
+        chord3 = 5,
+        chord4 = 10,
+        chord5 = 15,
+        chord6 = 20
+    )),
+    GuitarNote("D4", GuitarPositions(
+        chord2 = 3,
+        chord3 = 7,
+        chord4 = 12,
+        chord5 = 17
+    )),
+    GuitarNote("E4", GuitarPositions(
+        chord1 = 0,
+        chord2 = 5,
+        chord3 = 9,
+        chord4 = 14,
+        chord5 = 19
+    )),
+    GuitarNote("F4", GuitarPositions(
+        chord1 = 1,
+        chord2 = 6,
+        chord3 = 10,
+        chord4 = 15,
+        chord5 = 20
+    )),
+    GuitarNote("G4", GuitarPositions(
+        chord1 = 3,
+        chord2 = 8,
+        chord3 = 12,
+        chord4 = 17
+    )),
+    GuitarNote("A4", GuitarPositions(
+        chord1 = 5,
+        chord2 = 10,
+        chord3 = 14,
+        chord4 = 19
+    )),
+    GuitarNote("B4", GuitarPositions(
+        chord1 = 7,
+        chord2 = 12,
+        chord3 = 16
+    )),
+    GuitarNote("C5", GuitarPositions(
+        chord1 = 8,
+        chord2 = 13,
+        chord3 = 17
+    )),
+    GuitarNote("D5", GuitarPositions(
+        chord1 = 10,
+        chord2 = 15,
+        chord3 = 19
+    )),
+    GuitarNote("E5", GuitarPositions(
+        chord1 = 12,
+        chord2 = 17
+    )),
+    GuitarNote("F5", GuitarPositions(
+        chord1 = 13,
+        chord2 = 18
+    )),
+    GuitarNote("G5", GuitarPositions(
+        chord1 = 15,
+        chord2 = 20
+    )),
+    GuitarNote("A5", GuitarPositions(
+        chord1 = 17
+    )),
+    GuitarNote("B5", GuitarPositions(
+        chord1 = 19
+    )),
+    GuitarNote("C6", GuitarPositions(
+        chord1 = 20
+    ))
+)
+
+data class GuitarPositions(
+    val chord1: Int? = null,
+    val chord2: Int? = null,
+    val chord3: Int? = null,
+    val chord4: Int? = null,
+    val chord5: Int? = null,
+    val chord6: Int? = null
+)
 
 object TrebleClef {
     private const val highLineLimitPosition = 8
