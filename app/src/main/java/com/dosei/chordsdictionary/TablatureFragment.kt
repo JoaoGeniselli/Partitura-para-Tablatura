d51b93ff92c539ce5caccd7e23d156333616b6b0
JoaoGeniselli <joao.g.geniselli@gmail.com>
2020-08-07 23:23:41 -0300

    Tablature position layout adjustments.

--- app/src/main/java/com/dosei/chordsdictionary/TablatureFragment.kt
+++ app/src/main/java/com/dosei/chordsdictionary/TablatureFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.view_tablature_complete.*
 
@@ -24,13 +25,32 @@ class TablatureFragment : Fragment() {
         }
 
     private fun updatePositions() {
-        val positions = this.positions ?: return
-        val nonePosition = "X"
-        note_line_1.text = positions.chord1?.toString() ?: nonePosition
-        note_line_2.text = positions.chord2?.toString() ?: nonePosition
-        note_line_3.text = positions.chord3?.toString() ?: nonePosition
-        note_line_4.text = positions.chord4?.toString() ?: nonePosition
-        note_line_5.text = positions.chord5?.toString() ?: nonePosition
-        note_line_6.text = positions.chord6?.toString() ?: nonePosition
+        positions?.run {
+            updateAllChordPositions(
+                note_line_1 to chord1,
+                note_line_2 to chord2,
+                note_line_3 to chord3,
+                note_line_4 to chord4,
+                note_line_5 to chord5,
+                note_line_6 to chord6
+            )
+        }
+    }
+
+    private fun updateAllChordPositions(vararg pairs: Pair<TextView, Int?>) {
+        pairs.forEach { updatePosition(it.first, it.second) }
+    }
+
+    private fun updatePosition(lineLabel: TextView, position: Int?) {
+        position?.let { showPosition(lineLabel, it) } ?: hidePosition(lineLabel)
+    }
+
+    private fun showPosition(label: TextView, position: Int) {
+        label.visibility = View.VISIBLE
+        label.text = position.toString()
+    }
+
+    private fun hidePosition(label: TextView) {
+        label.visibility = View.GONE
     }
 }
