727a5c3b3028855a62de3f8d6e54341d3b2e8867
JoaoGeniselli <joao.g.geniselli@gmail.com>
2020-08-08 00:02:02 -0300

    Passing main activity's content to a fragment.

--- app/src/main/java/com/dosei/music/scoreconverter/MainActivity.kt
+++ app/src/main/java/com/dosei/music/scoreconverter/MainActivity.kt
@@ -1,57 +1,23 @@
 package com.dosei.music.scoreconverter
 
 import android.os.Bundle
-import android.widget.SeekBar
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity(), SeekBar.OnSeekBarChangeListener {
+class MainActivity : AppCompatActivity() {
 
-    private lateinit var scoreFragment: ScoreFragment
-    private lateinit var tablatureFragment: TablatureFragment
-
-    private val notes = initNaturalNotes()
-    private lateinit var currentNote: GuitarNote
+    private lateinit var scoreConverterFragment: ScoreConverterFragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initConverter()
+    }
 
-        scoreFragment = ScoreFragment()
-        tablatureFragment = TablatureFragment()
+    private fun initConverter() {
+        scoreConverterFragment = ScoreConverterFragment()
 
         supportFragmentManager.beginTransaction()
-            .replace(R.id.notes_container, scoreFragment, "Score")
+            .replace(R.id.main_container, scoreConverterFragment, "ScoreConverter")
             .commit()
-
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.tablature_container, tablatureFragment, "Tablature")
-            .commit()
-
-        seek_bar.max = notes.size - 1
-        seek_bar.progress = 0
-        seek_bar.setOnSeekBarChangeListener(this)
-
-        currentNote = notes.first()
     }
-
-    override fun onResume() {
-        super.onResume()
-        updateCurrentNote(currentNote)
-    }
-
-    private fun updateCurrentNote(note: GuitarNote) {
-        currentNote = note
-        text_current_note.text = getString(R.string.current_note, note.name)
-        scoreFragment.notePosition = notes.lastIndex - notes.indexOf(note)
-        tablatureFragment.positions = note.positions
-    }
-
-    override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
-        val selectedNote = notes[seek_bar.progress]
-        updateCurrentNote(selectedNote)
-    }
-
-    override fun onStartTrackingTouch(p0: SeekBar?) = Unit
-    override fun onStopTrackingTouch(p0: SeekBar?) = Unit
 }
---
+++ app/src/main/java/com/dosei/music/scoreconverter/ScoreConverterFragment.kt
@@ -0,0 +1,68 @@
+package com.dosei.music.scoreconverter
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.SeekBar
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_score_converter.*
+import kotlinx.android.synthetic.main.fragment_score_converter.seek_bar
+
+class ScoreConverterFragment : Fragment(), SeekBar.OnSeekBarChangeListener {
+
+    private lateinit var scoreFragment: ScoreFragment
+    private lateinit var tablatureFragment: TablatureFragment
+    private val notes = initNaturalNotes()
+    private lateinit var currentNote: GuitarNote
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? = inflater.inflate(R.layout.fragment_score_converter, container, false)
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        scoreFragment = ScoreFragment()
+        tablatureFragment = TablatureFragment()
+
+        activity?.supportFragmentManager?.run {
+            beginTransaction()
+                .replace(R.id.notes_container, scoreFragment, "Score")
+                .commit()
+
+            beginTransaction()
+                .replace(R.id.tablature_container, tablatureFragment, "Tablature")
+                .commit()
+        }
+
+        seek_bar?.apply {
+            max = notes.size - 1
+            progress = 0
+            setOnSeekBarChangeListener(this@ScoreConverterFragment)
+        }
+
+        currentNote = notes.first()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        updateCurrentNote(currentNote)
+    }
+
+    override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
+        val selectedNote = notes[seek_bar.progress]
+        updateCurrentNote(selectedNote)
+    }
+
+    private fun updateCurrentNote(note: GuitarNote) {
+        currentNote = note
+        text_current_note.text = getString(R.string.current_note, note.name)
+        scoreFragment.notePosition = notes.lastIndex - notes.indexOf(note)
+        tablatureFragment.positions = note.positions
+    }
+
+    override fun onStartTrackingTouch(p0: SeekBar?) = Unit
+    override fun onStopTrackingTouch(p0: SeekBar?) = Unit
+}
