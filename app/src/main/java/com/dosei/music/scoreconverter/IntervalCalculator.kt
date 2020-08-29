c5c7db4a3f117e23179d4cfddd4ff54d0ce2e989
JoaoGeniselli <joao.g.geniselli@gmail.com>
2020-08-29 11:07:29 -0300

    Dead code removal.

--- app/src/main/java/com/dosei/music/scoreconverter/IntervalCalculator.kt
+++
@@ -1,8 +0,0 @@
-package com.dosei.music.scoreconverter
-
-class IntervalCalculator {
-
-    fun calculateSemitonesBetween(initialNote: OctavedNote, finalNote: OctavedNote): Int {
-        return finalNote.absolutePosition - initialNote.absolutePosition
-    }
-}
