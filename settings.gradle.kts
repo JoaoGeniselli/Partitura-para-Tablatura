1bbab4d95fcf1cf22903ac9196fd0a7e96b41c9b
joao.geniselli.ext <2rfbeo5rjatrmxuulyailke44opjg4sj4cil63g56okdlup36v6a>
2021-04-14 10:12:45 -0300

    Tuner module setup.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,4 @@
+include(":tuner")
 rootProject.name = "Chords Dictionary"
 include(":app")
 include(":ui")
---
+++ tuner/src/main/java/com/dosei/music/scoreconverter/tuner/SoundRecognizer.kt
@@ -0,0 +1,14 @@
+package com.dosei.music.scoreconverter.tuner
+
+class NoteRecognizer() {
+
+    fun addNoteObserver(listener: NoteObserver) {
+
+    }
+
+    fun removeObserver(listener: NoteObserver) {
+
+    }
+}
+
+typealias NoteObserver = (Double) -> Unit
