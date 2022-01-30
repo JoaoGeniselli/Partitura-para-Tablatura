acf19b0562b29fb24c51a1826f6ccceef90f26bb
João <joao.g.geniselli@gmail.com>
2022-01-30 17:40:58 -0300

    Composable score.

--- ui/src/main/java/com/dosei/music/scoreconverter/ui/view/ComposableScore.kt
+++ ui/src/main/java/com/dosei/music/scoreconverter/ui/view/ComposableScore.kt
@@ -2,6 +2,7 @@ package com.dosei.music.scoreconverter.ui.view
 
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -9,77 +10,86 @@ import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.drawscope.inset
+import androidx.compose.ui.graphics.drawscope.rotate
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.dosei.music.scoreconverter.ui.view.NotationNotes.*
 
-object NotationNotes {
-    const val B6 = 32
-    const val A6 = 31
-    const val G6 = 30
-    const val F6 = 29
-    const val E6 = 28
-    const val D6 = 27
-    const val C6 = 26
-    const val B5 = 25
-    const val A5 = 24
-    const val G5 = 23
-    const val F5 = 22
-    const val E5 = 21
-    const val D5 = 20
-    const val C5 = 19
-    const val B4 = 18
-    const val A4 = 17
-    const val G4 = 16
-    const val F4 = 15
-    const val E4 = 14
-    const val D4 = 13
-    const val C4 = 12
-    const val B3 = 11
-    const val A3 = 10
-    const val G3 = 9
-    const val F3 = 8
-    const val E3 = 7
-    const val D3 = 6
-    const val C3 = 5
-    const val B2 = 4
-    const val A2 = 3
-    const val G2 = 2
-    const val F2 = 1
-    const val E2 = 0
+enum class NotationNotes(val index: Int, val isMainLine: Boolean, val isLine: Boolean) {
+    B6(32, false, false),
+    A6(31, false, true),
+    G6(30, false, false),
+    F6(29, false, true),
+    E6(28, false, false),
+    D6(27, false, true),
+    C6(26, false, false),
+    B5(25, false, true),
+    A5(24, false, false),
+    G5(23, false, true),
+    F5(22, false, false),
+    E5(21, false, true),
+    D5(20, false, false),
+    C5(19, false, true),
+    B4(18, false, false),
+    A4(17, false, true),
+    G4(16, false, false),
+    F4(15, true, true),
+    E4(14, false, false),
+    D4(13, true, true),
+    C4(12, false, false),
+    B3(11, true, true),
+    A3(10, false, false),
+    G3(9, true, true),
+    F3(8, false, false),
+    E3(7, true, true),
+    D3(6, false, false),
+    C3(5, false, true),
+    B2(4, false, false),
+    A2(3, false, true),
+    G2(2, false, false),
+    F2(1, false, true),
+    E2(0, false, false);
+
+    companion object {
+
+        fun getAll(intRange: IntRange): List<NotationNotes> {
+            return values().filter { intRange.contains(it.index) }.sortedBy { it.index }
+        }
+
+        fun totalNotes() = values().size
+    }
 }
 
 @Composable
-fun ComposableScore(modifier: Modifier = Modifier, noteRange: IntRange) {
+fun ComposableScore(modifier: Modifier = Modifier, noteRange: IntRange, noteIndex: Int? = null) {
+    val notes = NotationNotes.getAll(noteRange)
 
     Canvas(modifier = modifier) {
-        val spaceSize = size.height / (noteRange.last - noteRange.first)
+        val noteSize = 16.dp.toPx()
+        val stroke = 1.dp.toPx()
+        var yCursor = 0f
 
-        inset(spaceSize) {
-            noteRange.forEach {
+        notes.sortedByDescending { it.index }.forEach {
+            val yPosition = yCursor + (noteSize / 2f)
+            if (it.isLine) {
                 drawLine(
-                    color = Color.Black,
-                    start = Offset(x = 0f, y = it * spaceSize),
-                    end = Offset(x = size.width, y = it * spaceSize)
+                    color = if (it.isMainLine) Color.Black else Color.LightGray,
+                    strokeWidth = stroke,
+                    start = Offset(x = 0f, y = yPosition),
+                    end = Offset(x = size.width, y = yPosition)
                 )
             }
+
+            if (noteIndex == it.index) {
+                drawOval(
+                    color = Color.Black,
+                    topLeft = Offset(x = size.width / 2f, y = yCursor),
+                    size = Size(width = noteSize + 4.dp.toPx(), height = noteSize)
+                )
+            }
+            yCursor = yPosition
+
         }
-
-
-    }
-}
-
-class Geometry(
-    val scoreStart: Offset,
-    val scoreEnd: Offset,
-    val scoreSize: Size
-) {
-
-    fun centerOfLine(line: Int) {
-
-    }
-
-    fun centerOfSpace(space: Int) {
-
     }
 }
 
@@ -88,8 +98,11 @@ class Geometry(
 private fun PreviewComposableScore() {
     Surface(modifier = Modifier.fillMaxSize(), color = Color.White) {
         ComposableScore(
-            modifier = Modifier.fillMaxSize(),
-            noteRange = NotationNotes.E2..NotationNotes.B6
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            noteRange = E2.index..B6.index,
+            noteIndex = E4.index
         )
     }
 }
