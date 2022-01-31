7aa1806185b87a2fd074dd9a7ce6c9bb7af9ebc7
João <joao.g.geniselli@gmail.com>
2022-01-31 08:20:59 -0300

    Milestone.

--- ui/src/main/java/com/dosei/music/scoreconverter/ui/view/ComposableTablature.kt
+++ ui/src/main/java/com/dosei/music/scoreconverter/ui/view/ComposableTablature.kt
@@ -6,9 +6,11 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -16,38 +18,49 @@ import androidx.compose.ui.unit.dp
 fun ComposableTablature(modifier: Modifier = Modifier, strings: Int = 6, positions: Map<Int, Int>) {
     Canvas(modifier = modifier) {
         val positionSize = 16.dp.toPx()
+        val itemPadding = 2.dp.toPx()
+        val lineSize = positionSize + itemPadding * 2f
         val stroke = 1.dp.toPx()
         var yCursor = 0f
-        val itemPadding = 2.dp.toPx()
 
         val startX = 16.dp.toPx()
         val endX = size.width - 16.dp.toPx()
 
 
         for (string in 1..strings) {
-            val yPosition = yCursor + positionSize + itemPadding
+            val rectY = yCursor + itemPadding
+            val lineY = rectY + (positionSize / 2)
+
 
             drawLine(
                 color = Color.Black,
                 strokeWidth = stroke,
-                start = Offset(x = startX, y = yPosition),
-                end = Offset(x = endX, y = yPosition)
+                start = Offset(x = startX, y = lineY),
+                end = Offset(x = endX, y = lineY)
             )
-            yCursor = yPosition
+
+            drawRoundRect(
+                color = Color.Black,
+                cornerRadius = CornerRadius(x = 4f, y = 4f),
+                topLeft = Offset(x = (size.width /2f) - 16.dp.toPx(), y = rectY),
+                size = Size(width = 32.dp.toPx(), height = positionSize),
+                style = Stroke(width = 1.dp.toPx())
+            )
+            yCursor += lineSize
         }
 
         drawLine(
             color = Color.Black,
             strokeWidth = stroke,
-            start = Offset(x = startX, y = positionSize + itemPadding),
-            end = Offset(x = startX, y = yCursor)
+            start = Offset(x = startX, y = (positionSize / 2f) + itemPadding),
+            end = Offset(x = startX, y = yCursor - itemPadding - (positionSize / 2f))
         )
 
         drawLine(
             color = Color.Black,
             strokeWidth = stroke,
-            start = Offset(x = endX, y = positionSize + itemPadding),
-            end = Offset(x = endX, y = yCursor)
+            start = Offset(x = endX, y = (positionSize / 2f) + itemPadding),
+            end = Offset(x = endX, y = yCursor - itemPadding - (positionSize / 2f))
         )
     }
 
