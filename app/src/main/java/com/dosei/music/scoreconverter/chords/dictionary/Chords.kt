c22f06efd80824eda6e2daa6e9e7381813738cfe
João Geniselli <joao.g.geniselli@gmail.com>
2022-03-05 18:08:43 -0300

    CAGED major chords.

--- app/src/main/java/com/dosei/music/scoreconverter/chords/dictionary/Chords.kt
+++ app/src/main/java/com/dosei/music/scoreconverter/chords/dictionary/Chords.kt
@@ -1,65 +1,103 @@
 package com.dosei.music.scoreconverter.chords.dictionary
 
 import androidx.compose.ui.text.AnnotatedString
+import com.dosei.music.arpeggio.Barre
 import com.dosei.music.arpeggio.Component
+import com.dosei.music.arpeggio.OpenString
+import com.dosei.music.arpeggio.Position
 
 object Chords {
 
-    val A = chord(
-        "A",
-        o(5),
-        i(4, 2),
-        m(3, 2),
-        r(2, 2),
-        o(1)
+    val A = listOf(
+        chord("A", Shapes.C(12)),
+        chord(
+            "A",
+            o(5),
+            i(4, 2),
+            m(3, 2),
+            r(2, 2),
+            o(1)
+        ),
+        chord("A", Shapes.G(5)),
+        chord("A", Shapes.E(5)),
+        chord("A", Shapes.D(7)),
     )
 
-    val B = Chord(
-        name = AnnotatedString("B"),
-        components = Shapes.A(2)
+    val B = listOf(
+        chord("B", Shapes.C(14)),
+        chord("B", Shapes.A(2)),
+        chord("B", Shapes.G(7)),
+        chord("B", Shapes.E(7)),
+        chord("B", Shapes.D(9)),
     )
 
-    val C = chord(
-        "C",
-        r(5, 3),
-        m(4, 2),
-        o(3),
-        i(2, 1),
-        o(1)
+    val C = listOf(
+        chord(
+            "C",
+            r(5, 3),
+            m(4, 2),
+            o(3),
+            i(2, 1),
+            o(1)
+        ),
+        chord("C", Shapes.A(3)),
+        chord("C", Shapes.G(8)),
+        chord("C", Shapes.E(8)),
+        chord("C", Shapes.D(10)),
     )
 
-    val D = chord(
-        "D",
-        o(4),
-        i(3, 2),
-        r(2, 3),
-        m(1, 2)
+    val D = listOf(
+        chord("D", Shapes.C(5)),
+        chord("D", Shapes.A(5)),
+        chord("D", Shapes.G(10)),
+        chord("D", Shapes.E(10)),
+        chord(
+            "D",
+            o(4),
+            i(3, 2),
+            r(2, 3),
+            m(1, 2)
+        )
     )
 
-    val E = chord(
-        "E",
-        o(6),
-        m(5, 2),
-        r(4, 2),
-        i(3, 1),
-        o(2),
-        o(1),
+    val E = listOf(
+        chord("E", Shapes.C(7)),
+        chord("E", Shapes.A(7)),
+        chord("E", Shapes.G(12)),
+        chord(
+            "E",
+            o(6),
+            m(5, 2),
+            r(4, 2),
+            i(3, 1),
+            o(2),
+            o(1),
+        ),
+        chord("E", Shapes.D(2))
     )
 
-
-    val F = Chord(
-        name = AnnotatedString("F"),
-        components = Shapes.E(1)
+    val F = listOf(
+        chord("F", Shapes.C(8)),
+        chord("F", Shapes.A(8)),
+        chord("F", Shapes.G(13)),
+        chord("F", Shapes.E(1)),
+        chord("F", Shapes.D(3))
     )
 
-    val G = chord(
-        "G",
-        m(6, 3),
-        i(5, 2),
-        o(4),
-        o(3),
-        o(2),
-        r(1, 3)
+    val G = listOf(
+        chord("G", Shapes.C(10)),
+        chord("G", Shapes.A(10)),
+        chord(
+            "G",
+            m(6, 3),
+            i(5, 2),
+            o(4),
+            o(3),
+            o(2),
+            r(1, 3)
+        ),
+        chord("G", Shapes.E(3)),
+        chord("G", Shapes.D(5))
     )
 
     val Am = chord(
@@ -216,14 +254,22 @@ object Chords {
         p(1, 6),
     )
 
-    val all = listOf(
-        A, B, C, D, E, F, G,
+    val all = (A + B + C + D + E + F + G + listOf(
         Am, Bm, Cm, Dm, Em, Fm, Gm,
         A7, B7, C7, D7, E7, F7, G7,
         ADim, BDim, CDim, DDim, EDim, FDim, GDim,
 
         // _2, _3, _4, _5,
-    )
+    )).sortedBy { it.firstFret }
+
+    val Chord.firstFret: Int
+        get() = components.minOf {
+            when (it) {
+                is Position -> it.fret
+                is Barre -> it.fret
+                else -> 0
+            }
+        }
 
     private fun chord(name: AnnotatedString, vararg components: Component): Chord {
         return Chord(name = name, components = components.toList())
