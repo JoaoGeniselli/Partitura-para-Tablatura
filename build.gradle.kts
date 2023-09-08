038ad56eb93580ea6fbd1c337c3718c105fa2ffc
João Geniselli <joao.g.geniselli@gmail.com>
2023-09-07 22:05:30 -0300

    Removes jcenter and centralizes plugin.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,24 +1,22 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
 buildscript {
-    val kotlin_version by extra("1.6.21")
     repositories {
         google()
-        jcenter()
+        mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:8.1.1")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.21")
-        classpath("com.google.gms:google-services:4.3.10")
-        classpath("com.google.firebase:firebase-crashlytics-gradle:2.5.2")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
+        classpath(libs.gradle.plugin)
+        classpath(libs.kotlin.gradle)
+        classpath(libs.google.play.services)
+        classpath(libs.firebase.crashlytics.gradle)
     }
 }
 
 allprojects {
     repositories {
         google()
-        jcenter()
+        mavenCentral()
         maven(url = "https://jitpack.io")
         maven(url = "https://maven.pkg.github.com/JoaoGeniselli/ktransposer") {
             val secrets = SecretsAPI.forProject(rootProject)
