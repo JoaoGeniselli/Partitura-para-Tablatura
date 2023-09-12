6941b990b49eb1ae06effcf3282bc47b7c419375
João Geniselli <joao.g.geniselli@gmail.com>
2023-09-12 10:25:26 -0300

    Transposer adjustments.

--- app/src/main/java/com/dosei/music/scoreconverter/feature/chords/transposer/Transposer.kt
+++ app/src/main/java/com/dosei/music/scoreconverter/feature/chords/transposer/Transposer.kt
@@ -1,28 +1,17 @@
 package com.dosei.music.scoreconverter.feature.chords.transposer
 
-import android.widget.Toast
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Done
-import androidx.compose.material3.Badge
 import androidx.compose.material3.BottomAppBar
-import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FilledIconButton
-import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.FloatingActionButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.LocalTextStyle
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -31,28 +20,17 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.pluralStringResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.TextFieldValue
-import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.dosei.music.scoreconverter.R
 import com.dosei.music.scoreconverter.main.Feature
 import com.dosei.music.scoreconverter.toolbox.AdvertView
-import com.dosei.music.scoreconverter.toolbox.formatSemitonesToTones
-import com.dosei.music.scoreconverter.ui.view.Icon
-import com.dosei.music.scoreconverter.ui.view.ToggleIconRow
 import org.koin.java.KoinJavaComponent.get
 
 @Composable
@@ -66,7 +44,6 @@ fun TransposerLoader(
     Transposer(
         modifier = modifier,
         song = song.value,
-        semitones = semitones.value,
         onChangeSong = { song.value = it },
         onAddSemitone = { semitones.value += 1 },
         onRemoveSemitone = { semitones.value -= 1 },
@@ -88,7 +65,6 @@ fun TransposerLoader(
 fun Transposer(
     modifier: Modifier = Modifier,
     song: TextFieldValue,
-    semitones: Int,
     onChangeSong: (TextFieldValue) -> Unit,
     onAddSemitone: () -> Unit,
     onRemoveSemitone: () -> Unit,
@@ -103,19 +79,6 @@ fun Transposer(
         contentWindowInsets = WindowInsets(16.dp, 16.dp, 16.dp, 16.dp),
         bottomBar = {
             BottomAppBar(
-                floatingActionButton = {
-                    Badge {
-
-                    }
-                    FloatingActionButton(
-                        onClick = onTranspose,
-                    ) {
-                        Icon(
-                            imageVector = Icons.Default.Done,
-                            contentDescription = stringResource(R.string.action_transpose)
-                        )
-                    }
-                },
                 actions = {
                     IconButton(onClick = onBeautify) {
                         Icon(
@@ -129,13 +92,15 @@ fun Transposer(
                             contentDescription = stringResource(id = R.string.transposer_copy_button)
                         )
                     }
-                    IconButton(onClick = onRemoveSemitone) {
+                    IconButton(
+                        onClick = { onRemoveSemitone(); onTranspose() }
+                    ) {
                         Icon(
                             painter = painterResource(id = R.drawable.ic_baseline_remove_24),
                             contentDescription = stringResource(id = R.string.transposer_remove_semitone_button)
                         )
                     }
-                    IconButton(onClick = onAddSemitone) {
+                    IconButton(onClick = { onAddSemitone(); onTranspose() }) {
                         Icon(
                             painter = painterResource(id = R.drawable.ic_baseline_add_24),
                             contentDescription = stringResource(id = R.string.transposer_add_semitone_button)
@@ -146,8 +111,6 @@ fun Transposer(
         }
     ) { padding ->
         Column(modifier.padding(padding)) {
-            AdvertView(unitId = R.string.admob_transposer_banner_id)
-            Spacer(Modifier.height(8.dp))
             OutlinedTextField(
                 modifier = Modifier
                     .fillMaxSize()
@@ -155,7 +118,6 @@ fun Transposer(
                 textStyle = LocalTextStyle
                     .current
                     .copy(fontFamily = FontFamily.Monospace),
-                label = { Text(text = stringResource(R.string.chords_lyrics)) },
                 placeholder = {
                     Text(
                         fontFamily = FontFamily.Monospace,
@@ -164,16 +126,9 @@ fun Transposer(
                 },
                 value = song,
                 onValueChange = { content -> onChangeSong(content) },
-                supportingText = {
-                    Text(
-                        pluralStringResource(
-                            id = R.plurals.tones,
-                            count = (semitones / 2),
-                            semitones.formatSemitonesToTones()
-                        )
-                    )
-                }
             )
+            Spacer(Modifier.height(8.dp))
+            AdvertView(unitId = R.string.admob_transposer_banner_id)
             Spacer(Modifier.height(8.dp))
         }
     }
@@ -186,7 +141,6 @@ private fun PreviewTransposer() {
         Transposer(
             modifier = Modifier,
             song = TextFieldValue(),
-            semitones = 0,
             onChangeSong = {},
             onAddSemitone = {},
             onRemoveSemitone = {},
